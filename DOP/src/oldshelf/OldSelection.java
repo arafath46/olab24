6d16f9832b64331b0a9b8b9902d6f1306c464932
arafath46 <nparafath07@gmail.com>
2024-11-03 16:16:26 +0530

    Update OldSelection.java

--- DOP/src/oldshelf/OldSelection.java
+++ DOP/src/oldshelf/OldSelection.java
@@ -1,29 +1,31 @@
 package oldshelf;
-public class OldSelection {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-   
-    public static String getAgeOrTitle(Object o) {
-        if (!(o instanceof Book)) {
-            return "";
-        }
-        if (o instanceof Comic) {
-            return ((Comic) o).getTitle();
-        } else if (o instanceof Fiction) {
-            return ((Fiction) o).getName();
-        } else if (o instanceof TextBook) {
-            return ((TextBook) o).getSubject();
-        }
-        return "";
+class OldSelectionTest {
+
+    @Test
+    void testGetAgeOrTitleWithComic() {
+        Comic comic = new Comic("Dylan:The Great Adventures", 12);
+        assertEquals("Dylan:The Great Adventures", OldSelection.getAgeOrTitle(comic));
     }
-
-    public static void main(String[] args) {
-        TextBook t = new TextBook("Social Studies");
-        System.out.println("Subject of the TextBook: " + getAgeOrTitle(t)); 
-        
-        Comic c = new Comic("Dylan:The Great Adventures", 12);
-        System.out.println("Title of the Comic: " + getAgeOrTitle(c));
-        
-        Fiction f = new Fiction("Pulp Fiction", FictionType.Comedy);
-        System.out.println("Name of the Fiction: " + getAgeOrTitle(f));
+    @Test
+    void testGetAgeOrTitleWithTextBook() {
+        TextBook textBook = new TextBook("Social Studies");
+        assertEquals("Social Studies", OldSelection.getAgeOrTitle(textBook));
+    }
+    @Test
+    void testGetAgeOrTitleWithFiction() {
+        Fiction fiction = new Fiction("Pulp Fiction", FictionType.Comedy);
+        assertEquals("Pulp Fiction", OldSelection.getAgeOrTitle(fiction));
+    }
+    @Test
+    void testGetAgeOrTitleWithNonBookObject() {
+        Object nonBook = new Object();
+        assertEquals("", OldSelection.getAgeOrTitle(nonBook));
+    }
+    @Test
+    void testGetAgeOrTitleWithNull() {
+        assertEquals("", OldSelection.getAgeOrTitle(null));
     }
 }
